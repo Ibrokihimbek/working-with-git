957102f457657b396a11d976dabe44e203c99a3a
Abdurauf <raufasrayev1@gmail.com>
2022-11-24 18:38:29 +0500

    add: splash page

--- lib/screens/app_routs.dart
+++ lib/screens/app_routs.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:working_with_git/screens/home/home_page.dart';
+import 'package:working_with_git/screens/splash/splash_page.dart';
 
 abstract class RoutName {
   static const splash = 'splash';
@@ -18,8 +19,8 @@ abstract class RoutName {
 class AppRoutes {
   static Route generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      // case RoutName.splash:
-      //   return MaterialPageRoute(builder: (_) => SplashPage());
+      case RoutName.splash:
+        return MaterialPageRoute(builder: (_) => SplashPage());
       // case RoutName.login:
       //   return MaterialPageRoute(builder: (_) => LoginPage());
       // case RoutName.register:
--- lib/utils/app_icon.dart
+++ lib/utils/app_icon.dart
@@ -5,4 +5,8 @@ class AppIcon {
   static const icon_basket = "assets/icons/icon_basket.svg";
   static const icon_search = 'assets/icons/icon_search.svg';
   static const icon_voice = 'assets/icons/icon_voice.svg';
+
+  // Image
+
+  static const splash_image = 'assets/splash/splash_image.svg';
 }
